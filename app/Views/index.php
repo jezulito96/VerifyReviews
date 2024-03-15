0dc1ad2614061e830899523cf3d2bbc38c0b10ab
jezulito96 <jesus.g.9696@gmail.com>
2024-03-15 19:59:30 +0100

    p

--- app/Views/index.php
+++ app/Views/index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php //echo "<script src='". base_url() . "jquery/jquery.js' > </script>"; ?>
+    <?php echo "<script type='text/javascript' src='". base_url() . "jquery/jquery.js' > </script>"; ?>
     
-    <?php echo '<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>'; ?>
+    <?php //echo '<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>'; ?>
     <?php echo "<script src='". base_url() . "archivosJs/prueba.js' > </script>"; ?>
     <?php echo "<link rel='stylesheet' href='". base_url() . "css/index.css' />"; ?>
 
