79eab1e343726832005f322a8c6ea30e92e37c95
jezulito96 <jesus.g.9696@gmail.com>
2024-03-15 19:06:53 +0100

    modified:   app/Config/App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,8 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost:8080/verifyreviews/public';
+    // public string $baseURL = 'http://localhost:8080/';
+    public string $baseURL = 'http://verifyreviews.es';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
