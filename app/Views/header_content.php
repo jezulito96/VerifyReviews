d536a3c3467f914ed8fdc1ca5e03794b283b5188
jezulito96 <jesus.g.9696@gmail.com>
2024-03-16 19:48:18 +0100

    p

--- app/Views/header_content.php
+++ app/Views/header_content.php
@@ -1,30 +1,7 @@
 <div id="menuContainer">
     <?php   
-        if(isset($jsonContent)){
-            
-            $jsonData = json_decode($jsonContent, true);
 
-            if ($jsonData !== null) {
-                // Iterar sobre las capas del JSON
-                foreach ($jsonData['layers'] as $layer) {
-                    // Verificar si la capa es la capa del menú
-                    if ($layer['nm'] === 'menu') {
-                        // Iterar sobre las formas de la capa
-                        foreach ($layer['shapes'] as $shape) {
-                            // Verificar si la forma es una imagen
-                            if ($shape['ty'] === 4) {
-                                // Imprimir la ruta de la imagen
-                                echo '<img src="' . $shape['ks']['k'][0]['s']['i'][0] . '" alt="' . $shape['nm'] . '">';
-                            }
-                        }
-                    }
-                }
-            } else {
-                echo 'Error al decodificar el JSON.';
-            }
-        }else{
-            echo "naddaaa";
-        }
-
+        
+        echo '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAT0lEQVR4nO3WwQkAIAwDwIzezesWKuEO8jf4aBIAHpmSZEuS1w9QJK0/MiUBAPLBRTbj0zoatySpKTIlAQDywUU249M6GrckqSkyJQEg1x3OPOvHECpbTAAAAABJRU5ErkJggg==">';
     ?>
 </div>
