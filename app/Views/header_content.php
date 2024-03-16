39d72ef60f66cb2ef15a71d839279447b7fc9580
jezulito96 <jesus.g.9696@gmail.com>
2024-03-16 19:26:53 +0100

    p

--- app/Views/header_content.php
+++ app/Views/header_content.php
@@ -2,8 +2,33 @@
     <?php   
         echo "Header";
 
-        // Cargar el contenido del archivo JSON
-        $jsonFile = file_get_contents(base_url() . 'img/imgMenu.json');
+        if(file_exists(base_url() . 'img/imgMenu.json')){
+            // si existe se carga
+            $jsonContent = file_get_contents($jsonFilePath);
+
+            $jsonData = json_decode($jsonContent, true);
+
+            // si se ha parseado
+            if ($jsonData !== null) {
+                
+                foreach ($jsonData['layers'] as $layer) {
+                    if (isset($layer['nm']) && $layer['nm'] === 'menu') {
+                        foreach ($layer['shapes'] as $shape) {
+                            if (isset($shape['nm']) && $shape['nm'] === 'Path 1') {
+                                // Imprimir la ruta de la imagen
+                                echo '<img src="' . $shape['ks']['k'][0]['v'][0] . '" alt="Menu">';
+                            }
+                        }
+                    }
+                }
+
+            } else {
+                echo 'Error al parsear el JSON.';
+            }
+        }else{
+            echo "el archivo no existe";
+        }
+
 
         // Decodificar el contenido JSON
         $menuData = json_decode($jsonFile, true);
