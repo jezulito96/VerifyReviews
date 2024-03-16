fe86b500c6f0e125fa370945cb40ab2d6036a6ba
jezulito96 <jesus.g.9696@gmail.com>
2024-03-16 09:35:33 +0100

    p

--- app/Views/index.php
+++ app/Views/index.php
@@ -11,11 +11,9 @@
     <?php echo '<script type="text/javascript" src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>'; ?>
     
     <!-- Libreria jquery maps -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
     
-    <!-- Librerias para pintar mapas con libreria leaftlet.js-->
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
 
     <!-- Prueba de jqury -->
     <?php echo "<script src='". base_url() . "js/prueba.js' > </script>"; ?>
@@ -78,8 +76,10 @@
     <div id="mapa" class="mapa"></div>
 
 
-
-    <!-- Recoger ubicacion usuario -->
+    <!-- Librerias para pintar mapas con libreria leaftlet.js-->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <!-- Recoger ubicacion usuario y pintar mapa si acepta-->
     <?php echo "<script src='". base_url() . "js/location.js' > </script>"; ?>
 </body>
 </html>
