e840dfa0c0e5270ba582f944ca2413a38b2f957a
jezulito96 <jesus.g.9696@gmail.com>
2024-03-16 09:39:20 +0100

    p

--- public/js/location.js
+++ public/js/location.js
@@ -10,15 +10,6 @@ $(document).ready(function () {
                 // si acepta se pinta el mapa
                 $('#resultadoLocation').html('Tu ubicación actual es: Latitud ' + latitud + ' Longitud ' + longitud);
 
-                var map = L.map('mapa').setView([51.505, -0.09], 13);
-
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
-                
-                L.marker([51.5, -0.09]).addTo(map)
-                    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-                    .openPopup();
 
             }, function (error) {
                 switch (error.code) {
