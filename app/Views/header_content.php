ea0177763a1a6d301ed7654c8719c8efee24a35e
jezulito96 <jesus.g.9696@gmail.com>
2024-03-27 10:26:36 +0100

    p

--- app/Views/header_content.php
+++ app/Views/header_content.php
@@ -25,5 +25,5 @@
 <ul class="listaMenu" style="display: none">
     <li>Iniciar sesion</li>
     <li>Escribe tu reseña</li>
-    <li><a href="http://verifyReviews.es/verifyreviews/nuevoNegocio">¿Eres un negocio?</a></li>
+    <li><form action="nuevoNegocio" method="post"><input type="submit" value="¿Eres un negocio?"></form></li>
 </ul>
