59af3d33ce4fbd5e8faef71480a063e4401abcac
jezulito96 <jesus.g.9696@gmail.com>
2024-03-30 14:08:52 +0100

    prueba fotos creando directorios

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -75,7 +75,7 @@ class Home extends BaseController{
     }
 
     public function setNegocio(): string {
-        // $this->request->getPost('nombre');
+        $nombre = $this->request->getPost('nombre');
         // $this->request->getPost('email');
         // $this->request->getPost('calle');
         // $this->request->getPost('ciudad');
@@ -86,13 +86,37 @@ class Home extends BaseController{
         // $latitud = $this->request->getPost('latitud');
         // $longitud = $this->request->getPost('longitud');
 
+        $directorioNegocio = base_url() . "images/" . strtolower($nombre);
+        //compruebo si existe el nombre del negocio como carpeta en la carpeta de imagenes base()/images/nombreNegocio
+        if(!is_dir($directorioNegocio)) mkdir($directorioNegocio, 0777, true) ;
+        // si existe se comprueba que esten las carpetas de negocio y de resenas
+        //si no existen se crean 
+        if(!is_dir($directorioNegocio . "/negocio")) mkdir($directorioNegocio . "/negocio", 0777, true) ;
+        if(!is_dir($directorioNegocio . "/resenas")) mkdir($directorioNegocio. "/resenas", 0777, true) ;
+
+        //campo fotos de la base de datos para guardar nombre del archivo y extension
+        $fotosBD = "";
+
         //recibo imagenes
         if (isset($_FILES['fotos']) && !empty($_FILES['fotos']['name'][0])) {
             $numFotos = count($_FILES['fotos']['name']);
 
-            echo $numFotos;
-        }else{
-            echo "noo";
+            for ($i = 0; $i < $numFotos; $i++) {
+                $nombre_foto = "img" . ($i + 1);
+                $campoBD .= $nombre_foto . ":";
+
+                $tipoFoto = $_FILES['fotos']['type'][$i]; 
+                $campoBD .= $tipoFoto . "-";
+
+                $tmpFoto = $_FILES['fotos']['tmp_name'][$i]; 
+                        
+                if (move_uploaded_file($tmpFoto, $directorioNegocio . "/negocio/" . $nombre_foto)) {
+                    echo "todo guay";
+                } else {
+                    echo "todo feo";
+                }
+            }
+            
         }
 
         // if(!empty($latitud)) {
