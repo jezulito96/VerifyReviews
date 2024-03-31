ac46c5d1a92af3c3d95015a7a1115f2b7d89ef56
jezulito96 <jesus.g.9696@gmail.com>
2024-03-31 17:20:03 +0200

    p

--- app/Models/Emailmailer.php
+++ app/Models/Emailmailer.php
@@ -3,13 +3,7 @@
 namespace App\Models;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require FCPATH . 'vendor/phpmailer/phpmailer/src/Exception.php';
-require FCPATH . 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require FCPATH . 'vendor/phpmailer/phpmailer/src/SMTP.php';
-
-// require 'path/to/PHPMailer/src/Exception.php';
-// require 'path/to/PHPMailer/src/PHPMailer.php';
-// require 'path/to/PHPMailer/src/SMTP.php';
+require 'vendor/autoload.php';
 
 class Emailmailer {
     private $mail;
