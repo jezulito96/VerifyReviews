a881c064de6b90137242137f06ee4a5de8b907a7
jezulito96 <jesus.g.9696@gmail.com>
2024-03-31 15:46:54 +0200

    p

--- app/Models/Emailmailer.php
+++ app/Models/Emailmailer.php
@@ -4,11 +4,15 @@ namespace App\Models;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+
 class Emailmailer {
 
     public function __construct($destinatario, $asunto, $mensaje) {
         $correo = new PHPMailer(true);
-        // Configuración SMTP
+
         $correo->isSMTP();
         $correo->SMTPAuth = true; 
         $correo->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
@@ -19,14 +23,14 @@ class Emailmailer {
         $correo->Password = 'PwM}YKUx24i1$]HB'; 
         $correo->SMTPKeepAlive = true;
         $correo->Mailer = "smtp";
+        $correo ->Timeout = 30;
 
-        // Configuraciones del mensaje
         $correo->setFrom('verifyReviews@verifyreviews.es', 'VerifyReviews'); 
+        $correo ->isHTML(false);
         $correo->addAddress($destinatario); 
         $correo->Subject = $asunto; 
         $correo->Body = $mensaje; 
 
-        // Enviar el correo
         $correo->send();
 
 
