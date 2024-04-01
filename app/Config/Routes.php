2043bde89735d84e28b5c96151f814617e537158
jezulito96 <jesus.g.9696@gmail.com>
2024-04-01 18:29:00 +0200

    P

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,5 +26,5 @@ $routes->get('/confirmarEmail', 'Home::confirmarEmail');
 
 // vista de login
 $routes->get('/login', 'Home::vistaLogin');
-$routes->get('/setLogin', 'Home::setLogin');
+$routes->post('/setLogin', 'Home::setLogin');
 
