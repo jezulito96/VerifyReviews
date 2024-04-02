d87a8314fe9c9498bc7dadfacb06abd3cbe61f95
jezulito96 <jesus.g.9696@gmail.com>
2024-04-02 20:57:04 +0200

    p

--- app/Views/generarResenas.php
+++ app/Views/generarResenas.php
@@ -1,8 +1,4 @@
 <!-- el form para enviar es setGenerarResenas -->
-<?php
-    $sesion = session() -> get("sesionIniciada");
-    if(isset($sesion) && $sesion != 2 ||  !isset($sesion)) redirect('https://verifyreviews.es');
-?>
 <h3>Login</h3>
 <div class="containerformGenerarResena">
     <form action="setGenerarResenas" method="post" class="formLogin">
