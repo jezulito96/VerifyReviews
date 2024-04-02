5b6097d314e3f32f4fd93a632c7a47316e344498
jezulito96 <jesus.g.9696@gmail.com>
2024-04-02 11:38:34 +0200

    p

--- app/Views/login.php
+++ app/Views/login.php
@@ -6,7 +6,7 @@
 ?>
 <div class="containerformLogin">
     <form action="setLogin" method="post" class="formLogin">
-        <input type="email" name="contrasena" placeholder="Email" required>
+        <input type="email" name="email" placeholder="Email" required>
         <input type="password" name="contrasena" placeholder="Contraseña" required>
         <input type="submit" value="Iniciar Sesión">
     </form>
