1f7c31a5ef40cef1b4e265799186aae00464db28
jezulito96 <jesus.g.9696@gmail.com>
2024-04-04 19:31:17 +0200

    p

--- app/Views/header_content.php
+++ app/Views/header_content.php
@@ -60,33 +60,33 @@
 </nav> 
 
 <script>
-        // $(document).ready(funtion(){
+        $(document).ready(funtion(){
 
-        //     // menu desplegable
-        //     var imgMenu = $(".imgMenu");
-        //     var imgMenu2 = $(".imgMenu2");
-        //     var imgLogo = $(".imgLogo");
-        //     var cont = 1;
-        //     $(".menuContainer").click(function () {
+            // menu desplegable
+            var imgMenu = $(".imgMenu");
+            var imgMenu2 = $(".imgMenu2");
+            var imgLogo = $(".imgLogo");
+            var cont = 1;
+            $(".menuContainer").click(function () {
 
-        //         if (cont == 1) {
-        //             cont++;
-        //             $(".listaMenu").toggle("blind", 500);
-        //             imgMenu.toggle("clip", 500);
-        //             imgLogo.toggle();
-        //             setTimeout(function () {
-        //                 imgMenu2.toggle();
-        //             }, 500);
-        //         } else {
-        //             cont = 1;
-        //             $(".listaMenu").toggle("blind", 500);
-        //             imgMenu2.toggle("clip", 500);
-        //             setTimeout(function () {
-        //                 imgMenu.toggle();
-        //                 imgLogo.toggle();
-        //             }, 500);
-        //         }
-        //     });
+                if (cont == 1) {
+                    cont++;
+                    $(".listaMenu").toggle("blind", 500);
+                    imgMenu.toggle("clip", 500);
+                    imgLogo.toggle();
+                    setTimeout(function () {
+                        imgMenu2.toggle();
+                    }, 500);
+                } else {
+                    cont = 1;
+                    $(".listaMenu").toggle("blind", 500);
+                    imgMenu2.toggle("clip", 500);
+                    setTimeout(function () {
+                        imgMenu.toggle();
+                        imgLogo.toggle();
+                    }, 500);
+                }
+            });
 
-        // });
+        });
 </script>
