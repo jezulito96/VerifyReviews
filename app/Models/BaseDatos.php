79f85433169027dddc5bcd125fda3ba07d1f46fa
jezulito96 <jesus.g.9696@gmail.com>
2024-04-24 20:00:17 +0200

    p

--- app/Models/BaseDatos.php
+++ app/Models/BaseDatos.php
@@ -126,7 +126,7 @@ class BaseDatos extends Model
     }
 
     public function getUsuario($emailUsuario){
-        $orden = "SELECT * FROM usuario WHERE email=:email";
+        $orden = "SELECT * FROM usuario WHERE email=?";
         $parametros = [':email' => $emailUsuario];
         $consulta = $this -> db -> query($orden, $parametros);
         $numeroFilas = $consulta -> getNumRows();
