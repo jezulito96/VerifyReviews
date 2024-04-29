63684eee19b1dfee8f74fa59e6a0cd685175fe78
jezulito96 <jesus.g.9696@gmail.com>
2024-04-29 20:31:34 +0200

    p

--- app/Models/Qr.php
+++ app/Models/Qr.php
@@ -38,9 +38,8 @@ class Qr {
             'imageBase64'       => false,
             'imageTransparent'  => false,
             'moduleValues'      => [
-                // Datos del módulo en [R, G, B, A] (0-255); A es la opacidad
-                'dark'  => ['r' => 0, 'g' => 0, 'b' => 255, 'a' => 0],
-                'light' => ['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0],
+            'dark'  => ['r' => 0, 'g' => 0, 'b' => 0, 'a' => 255],
+            // 'light' => ['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0],
             ],
         ]);
 
