d48b6b966756644873154d06c14f1ba82ea55f1b
jezulito96 <jesus.g.9696@gmail.com>
2024-04-30 21:01:26 +0200

    p

--- app/Models/Qr.php
+++ app/Models/Qr.php
@@ -32,53 +32,53 @@ class Qr extends QRCode{
 
         $options = new QROptions;
 
-        $options->version             = 7;
-        $options->outputInterface     = QRImagick::class;
-        $options->imagickFormat       = 'webp';
-        $options->quality             = 90;
-        $options->scale               = 20;
-        $options->outputBase64        = false;
-        $options->bgColor             = '#ccccaa';
-        $options->imageTransparent    = true;
-        $options->transparencyColor   = '#ccccaa';
-        $options->drawLightModules    = true;
-        $options->drawCircularModules = true;
-        $options->circleRadius        = 0.4;
-        $options->keepAsSquare        = [
-            QRMatrix::M_FINDER_DARK,
-            QRMatrix::M_FINDER_DOT,
-            QRMatrix::M_ALIGNMENT_DARK,
-        ];
-        $options->moduleValues        = [
-            // finder
-            QRMatrix::M_FINDER_DARK    => '#A71111', // dark (true)
-            QRMatrix::M_FINDER_DOT     => '#A71111', // finder dot, dark (true)
-            QRMatrix::M_FINDER         => '#FFBFBF', // light (false)
-            // alignment
-            QRMatrix::M_ALIGNMENT_DARK => '#A70364',
-            QRMatrix::M_ALIGNMENT      => '#FFC9C9',
-            // timing
-            QRMatrix::M_TIMING_DARK    => '#98005D',
-            QRMatrix::M_TIMING         => '#FFB8E9',
-            // format
-            QRMatrix::M_FORMAT_DARK    => '#003804',
-            QRMatrix::M_FORMAT         => '#CCFB12',
-            // version
-            QRMatrix::M_VERSION_DARK   => '#650098',
-            QRMatrix::M_VERSION        => '#E0B8FF',
-            // data
-            QRMatrix::M_DATA_DARK      => '#4A6000',
-            QRMatrix::M_DATA           => '#ECF9BE',
-            // darkmodule
-            QRMatrix::M_DARKMODULE     => '#080063',
-            // separator
-            QRMatrix::M_SEPARATOR      => '#DDDDDD',
-            // quietzone
-            QRMatrix::M_QUIETZONE      => '#DDDDDD',
-        ];
-        
-        
-        $this -> cod_qr = (new QRCode($options))->render('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+$options->version             = 7;
+$options->outputInterface     = QRGdImagePNG::class;
+$options->scale               = 20;
+$options->outputBase64        = false;
+$options->bgColor             = [200, 150, 200];
+$options->imageTransparent    = true;
+#$options->transparencyColor   = [233, 233, 233];
+$options->drawCircularModules = true;
+$options->drawLightModules    = true;
+$options->circleRadius        = 0.4;
+$options->keepAsSquare        = [
+	QRMatrix::M_FINDER_DARK,
+	QRMatrix::M_FINDER_DOT,
+	QRMatrix::M_ALIGNMENT_DARK,
+];
+$options->moduleValues        = [
+	// finder
+	QRMatrix::M_FINDER_DARK    => [0, 63, 255], // dark (true)
+	QRMatrix::M_FINDER_DOT     => [0, 63, 255], // finder dot, dark (true)
+	QRMatrix::M_FINDER         => [233, 233, 233], // light (false), white is the transparency color and is enabled by default
+	// alignment
+	QRMatrix::M_ALIGNMENT_DARK => [255, 0, 255],
+	QRMatrix::M_ALIGNMENT      => [233, 233, 233],
+	// timing
+	QRMatrix::M_TIMING_DARK    => [255, 0, 0],
+	QRMatrix::M_TIMING         => [233, 233, 233],
+	// format
+	QRMatrix::M_FORMAT_DARK    => [67, 159, 84],
+	QRMatrix::M_FORMAT         => [233, 233, 233],
+	// version
+	QRMatrix::M_VERSION_DARK   => [62, 174, 190],
+	QRMatrix::M_VERSION        => [233, 233, 233],
+	// data
+	QRMatrix::M_DATA_DARK      => [0, 0, 0],
+	QRMatrix::M_DATA           => [233, 233, 233],
+	// darkmodule
+	QRMatrix::M_DARKMODULE     => [0, 0, 0],
+	// separator
+	QRMatrix::M_SEPARATOR      => [233, 233, 233],
+	// quietzone
+	QRMatrix::M_QUIETZONE      => [233, 233, 233],
+	// logo (requires a call to QRMatrix::setLogoSpace()), see QRImageWithLogo
+	QRMatrix::M_LOGO           => [233, 233, 233],
+];
+
+
+$this -> cod_qr = (new QRCode($options))->render('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
 
     }
