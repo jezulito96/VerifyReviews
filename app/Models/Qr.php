6c011f7fdaca665e6e9935e58ac60ab4f85c6f67
jezulito96 <jesus.g.9696@gmail.com>
2024-05-01 10:59:54 +0200

    p

--- app/Models/Qr.php
+++ app/Models/Qr.php
@@ -11,13 +11,10 @@ class Qr extends QRCode {
     private $cod_qr;
 
     public function __construct(){
-        $scale = 5;
-        $logoWidth = 50;
-        $logoHeight = 20;
         $options = new QROptions([
             'version'             => 6,
             'outputType'          => QROutputInterface::MARKUP_SVG,
-            'scale'               => $scale,
+            'scale'               => 5,
             'outputBase64'        => false,
             'eccLevel'            => QRCode::ECC_L,
             'addLogoSpace'        => true,
@@ -56,9 +53,7 @@ class Qr extends QRCode {
         $logoBase64 = base64_encode($logoData);
 
         // Insertar el logo en el SVG del código QR
-        // $this->cod_qr = str_replace('</svg>', '<image x="25" y="25" width="50px" height="50px" xlink:href="data:image/png;base64,'.$logoBase64.'" /></svg>', $codigoQRFinal);
-        // Calcular las coordenadas para centrar la imagen del logo dentro del código QR
-        $this->cod_qr = str_replace('</svg>', '<image x="'.(($options->scale * 25) - ($logoWidth / 2)).'" y="'.(($options->scale * 25) - ($logoHeight / 2)).'" width="'.$logoWidth.'" height="'.$logoHeight.'" xlink:href="data:image/png;base64,'.$logoBase64.'" /></svg>', $codigoQRFinal);
+        $this->cod_qr = str_replace('</svg>', '<image x="25" y="25" width="50" height="50" xlink:href="data:image/png;base64,'.$logoBase64.'" /></svg>', $codigoQRFinal);
 
         // Insertar el logo en el SVG del código QR
         // $this->cod_qr = str_replace('</svg>', '<image href="'.$logoPath.'" x="11" y="10" width="25px" height="25px" /></svg>', $codigoQRFinal);
