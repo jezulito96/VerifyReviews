ad8a8bd15d3de7103572a4f27a075506bdf6cc8b
jezulito96 <jesus.g.9696@gmail.com>
2024-05-01 16:55:03 +0200

    p

--- app/Views/head_content.php
+++ app/Views/head_content.php
@@ -127,66 +127,10 @@
     <!-- Prueba de jqury -->
     <?php echo "<script src='" . base_url() . "js/prueba.js' > </script>"; ?>
 
-    
-
-
-    <!-- Recoger ubicacion usuario si acepta obtener ubicacion-->
     <!-- Pinta el mapa dandole al boton de aceptar ubicacion -->
-    <?php //echo "<script src='". base_url() . "js/location.js' > </script>";        ?>
-
-
-    <!-- script para pintar los ticks de una nueva reseña -->
-    <?php echo "<script src='". base_url() . "js/ticksResenas.js' > </script>";        ?>
-
-
+    <?php echo "<script src='". base_url() . "js/location.js' > </script>";        ?>
 
     <!-- https://leafletjs.com/reference.html#map-factory -->
-    <script>
-        // $(document).ready(function () { 
-
-        //     $("#ubicacion").click(function () {
-        //         if (navigator.geolocation) {
-        //             // le pido al usuario acceder a su localizacion
-        //             navigator.geolocation.getCurrentPosition(function (position) {
-        //                 $('#ubicacion').toggle();
-        //                 var latitud = position.coords.latitude;
-        //                 var longitud = position.coords.longitude;
-
-
-        //                 $('#resultadoLocation').html('Tu ubicación actual es: Latitud ' + latitud + ' Longitud ' + longitud);
-
-        //                 // si acepta se pinta el mapa
-        //                 var mapa = L.map('mapa').setView([latitud, longitud], 16);
-
-        //                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        //                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        //                 }).addTo(mapa);
-
-        //                 L.marker([latitud, longitud]).addTo(mapa)
-        //                     .bindPopup('Nombre negocio <br> Dirección.') 
-        //                     .openPopup();
-
-        //             }, function (error) {
-        //                 switch (error.code) {
-        //                     case error.PERMISSION_DENIED:
-        //                         $('#resultadoLocation').html("El usuario denegó la solicitud de geolocalización.");
-        //                         break;
-        //                     case error.POSITION_UNAVAILABLE:
-        //                         $('#resultadoLocation').html("La información de ubicación no está disponible.");
-        //                         break;
-        //                     case error.TIMEOUT:
-        //                         $('#resultadoLocation').html("Se agotó el tiempo de espera para obtener la ubicación del usuario.");
-        //                         break;
-        //                     case error.UNKNOWN_ERROR:
-        //                         $('#resultadoLocation').html("Ocurrió un error desconocido al intentar obtener la ubicación.");
-        //                         break;
-        //                 }
-        //             });
-        //         } else {
-        //             $('#resultadoLocation').html("Tu navegador no soporta la geolocalización.");
-        //         }
-        //     });
-
-    </script>
-
+    <!-- script para pintar los ticks verdes de una nueva reseña -->
+    <?php echo "<script src='". base_url() . "js/ticksResenas.js' > </script>";        ?>
 </head>
--- public/js/location.js
+++ public/js/location.js
@@ -4,21 +4,22 @@ $(document).ready(function () {
         if (navigator.geolocation) {
             // le pido al usuario acceder a su localizacion
             navigator.geolocation.getCurrentPosition(function (position) {
+                $('#ubicacion').toggle();
                 var latitud = position.coords.latitude;
                 var longitud = position.coords.longitude;
 
-                
+
                 $('#resultadoLocation').html('Tu ubicación actual es: Latitud ' + latitud + ' Longitud ' + longitud);
 
                 // si acepta se pinta el mapa
-                var map = L.map('mapa').setView([51.505, -0.09], 13);
+                var mapa = L.map('mapa').setView([latitud, longitud], 16);
 
                 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
-                
-                L.marker([51.5, -0.09]).addTo(map)
-                    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+                }).addTo(mapa);
+
+                L.marker([latitud, longitud]).addTo(mapa)
+                    .bindPopup('Nombre negocio <br> Dirección.') 
                     .openPopup();
 
             }, function (error) {
