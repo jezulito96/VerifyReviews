ca412f9a451cfe6969062920fb9a350cd05f2381
jezulito96 <jesus.g.9696@gmail.com>
2024-05-01 10:33:19 +0200

    p

--- app/Models/Qr.php
+++ app/Models/Qr.php
@@ -48,7 +48,7 @@ class Qr extends QRCode {
         $logoPath = base_url() . 'img/logoMovil.png';
 
         // Insertar el logo en el SVG del código QR
-        $this->cod_qr = str_replace('</svg>', '<image href="'.$logoPath.'" x="3" y="3" width="60px" height="20px" /></svg>', $codigoQRFinal);
+        $this->cod_qr = str_replace('</svg>', '<image href="'.$logoPath.'" x="3" y="5" width="60px" height="20px" /></svg>', $codigoQRFinal);
         // $codigoQRFinal = str_replace('</svg>', '<image x="25" y="25" width="50" height="50" xlink:href="data:image/png;base64,'.$logoBase64.'" /></svg>', $codigoQRFinal);
     }
 
