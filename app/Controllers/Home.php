6601c6a76d235c5f6e45f96ed5a09a67f4a3e400
jezulito96 <jesus.g.9696@gmail.com>
2024-05-04 20:11:30 +0200

    p

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -446,7 +446,7 @@ class Home extends BaseController{
                 // Leer el archivo SVG
                 // $imagen->readImage($ruta_qr);
                 $imagen -> readImageBlob($imagen_qr);
-                // $imagen->setImageResolution(300, 300);
+                $imagen->setImageResolution(600);
                 // $imagen->resizeImage(200, 200, \Imagick::ALIGN_UNDEFINED, 1);
 
                 // Establecer el formato de salida como PNG
