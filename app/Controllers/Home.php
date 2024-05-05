8ceae103e07df6f0a6c82d8e6d744719480f2b5c
jezulito96 <jesus.g.9696@gmail.com>
2024-05-05 13:44:40 +0200

    p

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -61,7 +61,7 @@ class Home extends BaseController{
 
         // si no va con clave redirecciona a inicio
         if(!isset($claveCifradaHex) || empty($claveCifradaHex)) {
-            redirect('https://www.verifyreviews.es');
+            header('location:https://www.verifyreviews.es');
         }
         // Convertir la clave cifrada de hexadecimal a binario
         $clavePublica = hex2bin($claveCifradaHex);
