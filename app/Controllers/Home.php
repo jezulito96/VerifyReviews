6419f39f43637a05acb2906473f627e981a0ee77
jezulito96 <jesus.g.9696@gmail.com>
2024-05-10 20:41:57 +0200

    p

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -383,14 +383,7 @@ class Home extends BaseController{
         $es_sesion_resena = false;
         if(isset($_POST['es_sesion_resena'])){
             if(!isset($_POST['qr_key'])){
-                $master = Master::obtenerInstancia();
-                $maleta_index['listaCategorias'] = $master -> getListaCategorias();
-                
-                //vistas
-                $maleta['head_content'] = view('head_content');
-                $maleta['header_content'] = view('header_content'); 
-                $maleta['index_content'] = view('index_content', $maleta_index); 
-                return view('index', $maleta);
+                return redirect() -> to("http://verifyReviews.es");
             }
 
             if($_POST['es_sesion_resena'] == "sesion"){
