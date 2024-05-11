ce16c9cab807ba844868722d249d4a10ede70af7
jezulito96 <jesus.g.9696@gmail.com>
2024-05-11 19:24:41 +0200

    p

--- app/Models/BaseDatos.php
+++ app/Models/BaseDatos.php
@@ -194,14 +194,14 @@ class BaseDatos extends Model
 
     public function getMaxResena(){
 
-        $orden = "SELECT MAX(cod_resena) FROM resena ";
+        $orden = "SELECT MAX(cod_resena) as codigo FROM resena ";
         $consulta = $this -> db -> query($orden);
         $numeroFilas = $consulta -> getNumRows();
 
         if($numeroFilas > 0 ){
             $sql = $consulta -> getRow();
             
-            return $sql->cod_resena;
+            return $sql->codigo;
         }else{
             return 1;
         }
