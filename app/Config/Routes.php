514e7fd81838cc65b1db1540f40568152b577145
jezulito96 <jesus.g.9696@gmail.com>
2024-05-17 17:07:00 +0200

    p

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -41,4 +41,4 @@ $routes->post('/setGenerarResenas', 'Home::setGenerarResenas');
 
 
 // vista para mostrar la lista de negocios de una categoria
-$routes->post('/categorias', 'Home::vista_cat_negocio');
+$routes->get('/categorias', 'Home::vista_cat_negocio');
