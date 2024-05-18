8142d155d47466afcb317a810fd921a59e1032f9
jezulito96 <jesus.g.9696@gmail.com>
2024-05-18 12:58:01 +0200

    p

--- app/Views/cont_categoria.php
+++ app/Views/cont_categoria.php
@@ -2,10 +2,28 @@
 
     if(isset($lista_negocios)){
         foreach($lista_negocios as $i => $negocio){
-            echo "categoria: " . $negocio -> getCodCategoria() ." <br>";
-            echo "nombre Categoria: " . $negocio -> getNombreCategoria() ."<br>";
-            echo '<a href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >Info negocio</a><br>';
-            echo "nombre negocio: " . $negocio -> getNombre()."<br><hr>";
-            
+            // echo "categoria: " . $negocio -> getCodCategoria() ." <br>";
+            // echo "nombre Categoria: " . $negocio -> getNombreCategoria() ."<br>";
+            // echo '<a href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >Info negocio</a><br>';
+            echo "<h3>nombre negocio: " . $negocio -> getNombre()."<h3>";
+
+            echo '<div class="fotosContainerNegocio">';
+
+                // recojo las rutas de las fotos y las pinto 
+                $foto_principal = $negocio -> getFotoPrincipal();
+                echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
+                    echo '<img src="'. base_url(). $foto_principal .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
+                echo '</a>';
+
+
+                $rutasimgs = $negocio -> getFotoPrincipal();
+                $imagenes = explode(",", $rutasimgs);
+                foreach($imagenes as $key => $valor){
+                    echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
+                        echo '<img src="'. base_url(). $valor .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
+                    echo '</a>';
+                }
+            echo '</div>';
         }
     }
+    
