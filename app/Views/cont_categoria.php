99872720309c370793b9815316ac7c70f71cdcfb
jezulito96 <jesus.g.9696@gmail.com>
2024-05-18 13:07:28 +0200

    p

--- app/Views/cont_categoria.php
+++ app/Views/cont_categoria.php
@@ -5,26 +5,27 @@
             // echo "categoria: " . $negocio -> getCodCategoria() ." <br>";
             // echo "nombre Categoria: " . $negocio -> getNombreCategoria() ."<br>";
             // echo '<a href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >Info negocio</a><br>';
-            echo "<h3>nombre negocio: " . $negocio -> getNombre()."</h3>";
+            echo "<h3 class='tituloNegocio'>" . $negocio -> getNombre()."</h3>";
 
             echo '<div class="fotosContainerNegocio">';
 
                 // recojo las rutas de las fotos y las pinto 
                 $foto_principal = $negocio -> getFotoPrincipal();
-                // echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
                 echo '<div class="fotoContainer" >';
-                    echo '<img src="'. base_url(). $foto_principal .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
-                // echo '</a>';
+                    echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
+                        echo '<img src="'. base_url(). $foto_principal .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
+                    echo '</a>';
                 echo '</div>';
 
 
                 $rutasimgs = $negocio -> getFotoPrincipal();
                 $imagenes = explode(",", $rutasimgs);
                 foreach($imagenes as $key => $valor){
-                    // echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
+                   
                     echo '<div class="fotoContainer" >';
-                        echo '<img src="'. base_url(). $valor .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
-                    // echo '</a>';
+                        echo '<a class="fotoContainer"  href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >';
+                            echo '<img src="'. base_url(). $valor .'" alt="'. $negocio -> getNombre() .'" title="'. $negocio -> getNombre() .'"/>';
+                        echo '</a>';
                     echo '</div>';
                 }
                 
