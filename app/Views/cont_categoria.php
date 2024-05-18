e3c60d2d0e64943ae427f661436015c944fba57e
jezulito96 <jesus.g.9696@gmail.com>
2024-05-18 10:27:48 +0200

    p

--- app/Views/cont_categoria.php
+++ app/Views/cont_categoria.php
@@ -3,6 +3,6 @@
     if(isset($lista_negocios)){
         foreach($lista_negocios as $i => $negocio){
             echo "categoria: " . $negocio -> getCodCategoria() ." <br>";
-            echo "nombre Categoria: " . $negocio -> getNombreCategoria() "<br><hr>";
+            echo "nombre Categoria: " . $negocio -> getNombreCategoria() ."<br><hr>";
         }
     }
