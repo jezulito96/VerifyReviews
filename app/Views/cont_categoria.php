9763874c3d0b086ce9722dec6fac5f2bf4d22fdd
jezulito96 <jesus.g.9696@gmail.com>
2024-05-18 11:09:03 +0200

    p

--- app/Views/cont_categoria.php
+++ app/Views/cont_categoria.php
@@ -4,7 +4,8 @@
         foreach($lista_negocios as $i => $negocio){
             echo "categoria: " . $negocio -> getCodCategoria() ." <br>";
             echo "nombre Categoria: " . $negocio -> getNombreCategoria() ."<br>";
-            echo "nombre negocio: " . $negocio -> getNombre()."<br><hr>";
             echo '<a href="https://verifyreviews.es/verifyreviews/negocio?id='. $negocio -> getCodNegocio() . '" >Info negocio</a>';
+            echo "nombre negocio: " . $negocio -> getNombre()."<br><hr>";
+            
         }
     }
