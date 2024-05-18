3a63a3f1a3df88e7aab05f51c13b4513eaac77d2
jezulito96 <jesus.g.9696@gmail.com>
2024-05-18 10:11:49 +0200

    p

--- app/Models/Master.php
+++ app/Models/Master.php
@@ -58,7 +58,8 @@ class Master {
                 );
 
                 $baseDatos = new BaseDatos();
-                $nombre_categoria = $baseDatos -> getCategoria($negocio -> getCodCategoria());
+                $cat =  $baseDatos -> getCategoria($negocio -> getCodCategoria());
+                $nombre_categoria = $cat['tipo_negocio'];
                 $negocio -> setNombreCategoria($nombre_categoria);
                 $this->listaNegocios[] = $negocio;
             }
