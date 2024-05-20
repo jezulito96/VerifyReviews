ba675055fb6ac5a74ca2dc3d63308315976bb63f
jezulito96 <jesus.g.9696@gmail.com>
2024-05-20 20:51:09 +0200

    p

--- app/Views/info_negocio.php
+++ app/Views/info_negocio.php
@@ -146,60 +146,136 @@
             ?>
         </div>
 <script>
-    $(document).ready(function () {
+$(document).ready(function () {
+
+
+    // -------------    Para los botones de las reseñas fotos y opinion     -------------------
+    $(".btn_opinion1").click(function() {
         
-        // -------------    Para los botones de las reseñas fotos y opinion     -------------------
-        $(".btn_opinion1").click(function() {
-            
-                var id = $(this).attr('id');
-                var $popup = $("#popup_" + id);
-                var $boton_cerrar = $(".boton_cerrar_" + id);
-
-                $popup.css("display", "flex");
-
-                $boton_cerrar.click(function() {
-                $popup.css("display", "none");
-                });
-
-                $(window).click(function (evento) {
-                    if ($(evento.target).is($popup)) {
-                        $popup.css("display", "none");
-                    }
-                });
-        });
-
-
-        $(".btn_opinion2").click(function() {
             var id = $(this).attr('id');
+            var $popup = $("#popup_" + id);
+            var $boton_cerrar = $(".boton_cerrar_" + id);
 
-            var $popup2 = $("#popup2_" + id );
-            var $boton_cerrar2 = $(".boton_cerrar2_" + id);
+            $popup.css("display", "flex");
 
-            $popup2.css("display", "flex");
-           
-            $boton_cerrar2.click(function () {
-                $popup2.css("display", "none");
+            $boton_cerrar.click(function() {
+            $popup.css("display", "none");
             });
 
             $(window).click(function (evento) {
-                if ($(evento.target).is($popup2)) {
-                    $popup2.css("display", "none");
+                if ($(evento.target).is($popup)) {
+                    $popup.css("display", "none");
                 }
             });
+    });
 
+
+    $(".btn_opinion2").click(function() {
+        var id = $(this).attr('id');
+
+        var $popup2 = $("#popup2_" + id );
+        var $boton_cerrar2 = $(".boton_cerrar2_" + id);
+
+        $popup2.css("display", "flex");
+        
+        $boton_cerrar2.click(function () {
+            $popup2.css("display", "none");
         });
 
+        $(window).click(function (evento) {
+            if ($(evento.target).is($popup2)) {
+                $popup2.css("display", "none");
+            }
+        });
 
     });
+
+    // PINTAR EL Mapa  info_mapa
+    
+    $(document).ready(function () {
+
+    // Al cargar la página, hacer visible el botón y aplicar el efecto de desenfoque
+    $('#main').addClass('blur');
+    $('#header').addClass('blur');
+    $('#ubicacion').show();
+
+    $("#aceptarUbicacion").click(function () {
+        $("#containerPermisosUbicacion").hide();
+        $('#main').removeClass('blur');
+        $('#header').removeClass('blur');
+    });   
+
+    $("#aceptarUbicacion").click(function () {
+        $("#containerPermisosUbicacion").hide();
+        $('#main').removeClass('blur');
+        $('#header').removeClass('blur');
+
+
+    /////////////////////////////////////////////////// esto es lo bueno
+    // if (navigator.geolocation) {
+        // le pido al usuario acceder a su localizacion
+        // navigator.geolocation.getCurrentPosition(function (position) {
+            // $('#ubicacion').toggle();
+            var latitud = $("#latitud");
+            var longitud = $("#longitud");
+
+            var mapa = L.map('info_mapa').setView([latitud, longitud], 16);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(mapa);
+
+            L.marker([latitud, longitud]).addTo(mapa)
+                .bindPopup('Nombre negocio <br> Dirección.') 
+                .openPopup();
+
+            }
+        // , function (error) {
+        //     switch (error.code) {
+        //         case error.PERMISSION_DENIED:
+        //             $('#resultadoLocation').html("El usuario denegó la solicitud de geolocalización.");
+        //             break;
+        //         case error.POSITION_UNAVAILABLE:
+        //             $('#resultadoLocation').html("La información de ubicación no está disponible.");
+        //             break;
+        //         case error.TIMEOUT:
+        //             $('#resultadoLocation').html("Se agotó el tiempo de espera para obtener la ubicación del usuario.");
+        //             break;
+        //         case error.UNKNOWN_ERROR:
+        //             $('#resultadoLocation').html("Ocurrió un error desconocido al intentar obtener la ubicación.");
+        //             break;
+        //     }
+        // });
+    } else {
+        $('#resultadoLocation').html("Tu navegador no soporta la geolocalización.");
+    }
+
+});
 </script>
         <div id="content_mapa">
-            <h3>
-                Mapa
-            </h3>   
-            <div id="info_opiniones">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu erat ornare, maximus arcu sit amet, lacinia eros. Vivamus porta sem lectus, et vulputate lectus malesuada sit amet. Ut imperdiet condimentum ornare. Aenean id porttitor lorem. In lectus ipsum, porta id eros eget, congue tempus purus. Duis ut maximus lorem, ut semper sapien. Etiam dignissim bibendum tellus, consequat aliquam justo. Duis arcu felis, placerat vitae molestie eu, efficitur eget nisl. Mauris scelerisque nec enim sed elementum. Nam quis volutpat enim.
+            <div class="head_mapa">
+                <div class="icono_mapa">
+                    <i class="fas fa-map-marker-alt"></i>
+                </div>
+                <div class="calle_mapa">
+                    <?php echo $negocio -> getCalle() . ", " . $negocio -> getCiudad() . ", " . $negocio -> getPais(); ?> 
+                </div>
+            </div>
 
-            Aenean at quam malesuada ipsum viverra tristique. Phasellus erat lacus, imperdiet vitae libero eget, efficitur eleifend enim. Aenean et tristique metus. Maecenas id tortor eu risus pretium ornare. Integer eu ornare lacus, sed maximus odio. Integer id magna metus. Sed elementum, lorem nec gravida viverra, libero massa elementum sapien, vitae maximus risus ante ac sem. In a efficitur magna. Phasellus feugiat libero ipsum. Aliquam interdum sollicitudin fermentum. Praesent sit amet nunc turpis. Nulla cursus non est vel scelerisque. Suspendisse accumsan massa eu aliquam ornare. Nullam vel orci faucibus, volutpat leo eget, varius sem. In vitae sagittis lacus, non commodo mi. Quisque velit risus, tincidunt a porttitor ac, imperdiet vel dui. 
+            <div id="info_mapa" class="info_mapa" >
+                <?php
+                    $coordenadas = $negocio -> getCoordenadas();
+                    if($coordenadas == null ||  empty($coordenadas)){
+                        echo "Upss hemos tenido un problema con el mapa, en estos  momentos no es posible dar la ubicacion,perdone las molestias";
+                    }else{
+                        $coord = explode(",", $coordenadas);
+
+                        echo '<input type="hidden" value="'. $coord[0] . '" id="latitud" />';
+                        echo '<input type="hidden" value="'. $coord[1] . '" id="longitud" />';
+                    }
+
+                ?>
+                
             </div> 
         </div>
 
@@ -320,5 +396,7 @@
                 $('.flechaArriba').show();
             }
         });
+
+        
     });
 </script>
