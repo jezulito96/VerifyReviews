6b05803a223cc9c9dde34d401786b6d14baf9a0c
jezulito96 <jesus.g.9696@gmail.com>
2024-05-29 18:02:09 +0200

    p

--- app/Views/vista_footer.php
+++ app/Views/vista_footer.php
@@ -1,43 +1,46 @@
 <div class="footer">
 
-    <ul class="listaMenu_footer">
-        <img src="<?php echo base_url() ?>img/logoMovil.png" alt="Logotipo">
-        <?php
-            // $sesionIniciada = session() -> get("sesionIniciada");
-            // if(isset($sesionIniciada) && $sesionIniciada == 1) {
+    <img src="<?php echo base_url() ?>img/logoMovil.png" alt="Logotipo">
 
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/cerrarSesion">Cerrar sesion</a></li>';
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/misResenas">Mis reseñas</a></li>';
-            //     echo '<li>Favoritos</li>';
+    <ul class="listaMenu_footer">
+        <?php
+            $sesionIniciada = session() -> get("sesionIniciada");
+            if(isset($sesionIniciada) && $sesionIniciada == 1) {
+
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/cerrarSesion">Cerrar sesion</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/misResenas">Mis reseñas</a></li>';
+                echo '<li>Favoritos</li>';
                 
 
-            // } else if(isset($sesionIniciada) && $sesionIniciada == 2){
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/cerrarSesion">Cerrar sesion</a></li>';
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/miNegocio">Mi negocio</a></li>';
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/generarResenas">Generar reseñas</a></li>';
+            } else if(isset($sesionIniciada) && $sesionIniciada == 2){
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/cerrarSesion">Cerrar sesion</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/miNegocio">Mi negocio</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/generarResenas">Generar reseñas</a></li>';
 
-            // } else{
+            } else{
 
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/login">Iniciar sesion</a></li>';
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/nuevoNegocio">¿Eres un negocio?</a></li>';
-            //     echo '<li><a href="http://verifyReviews.es/verifyreviews/nuevoUsuario">Registrate</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/login">Iniciar sesion</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/nuevoNegocio">¿Eres un negocio?</a></li>';
+                echo '<li><a href="http://verifyReviews.es/verifyreviews/nuevoUsuario">Registrate</a></li>';
 
-            // }       
+            }       
         ?>
     </ul>   
 
-            
-            <nav>
-                <a href="#about">Sobre nosotros</a>
-                <a href="#contact">Contacto</a>
-                <a href="#privacy">Política de privacidad</a>
-            </nav>
-            <div class="social">
-                <a href="https://www.facebook.com" target="_blank">FB</a>
-                <a href="https://www.twitter.com" target="_blank">TW</a>
-                <a href="https://www.instagram.com" target="_blank">IG</a>
-            </div>
-            <p>&copy; 2024 TuEmpresa. Todos los derechos reservados.</p>
+    <div class="social">
+        <a href="https://www.facebook.com" target="_blank">      
+            <i class="fab fa-twitter iconos_redes"></i>
+        </a>
+        <a href="https://www.twitter.com" target="_blank">
+            <i class="fab fa-facebook-f iconos_redes"></i>
+        </a>
+        <a href="https://www.instagram.com" target="_blank">
+            <i class="fab fa-instagram iconosColor"></i>
+        </a>
+    </div>
+    
+    <p>&copy; 2024 Verify Reviews. Proyecto de DAW 2024.</p>
+
 </div>
 
 
