e46099d0aa6f579e9e8e45e629b189d3589cfe8f
jezulito96 <jesus.g.9696@gmail.com>
2024-06-01 03:27:10 +0200

    p

--- app/Views/vista_footer.php
+++ app/Views/vista_footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
     <div class="container_logo_footer">
-        <img src="<?php echo base_url() ?>img/logoMovil.png" alt="Logotipo">
+        <img src="<?php echo base_url() ?>img/logoMovil_2.png" alt="Logotipo">
     </div>
     
 
