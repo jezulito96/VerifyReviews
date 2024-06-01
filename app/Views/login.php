19fb93265fea7f6b4d8bac94b961c712ce8a724f
jezulito96 <jesus.g.9696@gmail.com>
2024-06-02 01:13:59 +0200

    p

--- app/Views/login.php
+++ app/Views/login.php
@@ -21,6 +21,7 @@
     <form action="setLogin" method="post" class="formLogin">
         <input type="email" name="email" placeholder="Email" class="inputs" required focus>
         <input type="password" name="contrasena" placeholder="Contraseña" class="inputs" required>
+        <small>Recordar contraseña</small>
         <div class="botones_enviar">
             <input type="submit" value="Iniciar Sesión" class="btn_enviar">
             <a href="http://verifyReviews.es/verifyreviews/nuevoUsuario" style="text-align:center;" class="btn_enviar2">Registrate</a>
