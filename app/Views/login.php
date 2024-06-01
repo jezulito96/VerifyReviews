fbaaae75bad3754bc3338a336d8922de04f0e258
jezulito96 <jesus.g.9696@gmail.com>
2024-06-01 18:10:00 +0200

    p

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,7 +7,7 @@
             $sesion = session() -> get("sesionIniciada");
             if(isset($sesion) && $sesion == 1 || isset($sesion) && $sesion == 2) {
                 $usuario = session() -> get('usuario_en_sesion');
-                var_dump($nombreUsuario);
+                var_dump($usuario);
                 echo '<p class="bien">Bienvenido, </p>';
             }elseif(isset($errorEmail)){
                 echo '<p class="error">' . $errorEmail . '</p>';
