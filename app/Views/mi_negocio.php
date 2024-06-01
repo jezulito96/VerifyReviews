fa0f47c065148f06b215880b14ccfb88b5333593
jezulito96 <jesus.g.9696@gmail.com>
2024-06-02 01:47:42 +0200

    p

--- app/Views/mi_negocio.php
+++ app/Views/mi_negocio.php
@@ -2,21 +2,25 @@
 
     <h1 style="padding-left:20px;">Mi negocio</h1>
 
-    <h5>Valoración</h5>
-    <?php
-    if(isset($error)){
-        echo $error;
-    }else{
-    
-        if(isset($estadisticas)){
-            echo '<div class="container_valoracion">';
-                echo "<h6>Valoracion de tus clientes:</h6>" . "<h5>" . round($estadisticas['nota_media'],1) . " / 5</h5>";
-            echo '</div>';
+
+    <section class="div_valoracion">
+        <h5>Valoración</h5>
+        <?php
+        if(isset($error)){
+            echo $error;
+        }else{
         
-    ?>
-    <h5>Ranking</h5>
-    <?php 
-        
+            if(isset($estadisticas)){
+                echo '<div class="container_valoracion">';
+                    echo "<h6>Valoracion de tus clientes:</h6>" . "<h5>" . round($estadisticas['nota_media'],1) . " / 5</h5>";
+                echo '</div>';
+            
+        ?>
+    </section>
+
+    <section class="div_ranking">
+        <h5>Ranking</h5>
+        <?php 
 
             foreach($estadisticas['ranking'] as $i => $posicion){
                 echo '<a href="https://verifyreviews.es/verifyreviews/negocio?id='. $posicion['cod_negocio'] . '" class="container_ranking">';
@@ -26,34 +30,36 @@
 
                 echo '</a>';
             }
+            
+        ?>
+    </section>
 
-        
-    ?>
 
+    <section class="div_tus_resenas">
 
-    <h5>Tus reseñas</h5>
-    <?php
-        }
-
-        if(isset($lista_resenas)){
-            echo "<ul>";
-            foreach($lista_resenas as $i => $resena){
-
-                echo "<li class='mi_resena'>";
-                    $fecha_completa = $resena -> getFechaServicio();
-                    $fecha = substr($fecha_completa, 0, 10);
-                    echo  $fecha . " - " .  $resena -> getNickname() ;
-
-                    echo '<div class="mi_resena_container" style="display:none;">Holaa</div>';
-
-                echo "</li>";
-
+        <h5>Tus reseñas</h5>
+        <?php
             }
-            echo "</ul>";
-        }
-    }
-    ?>
 
+            if(isset($lista_resenas)){
+                echo "<ul>";
+                foreach($lista_resenas as $i => $resena){
+
+                    echo "<li class='mi_resena'>";
+                        $fecha_completa = $resena -> getFechaServicio();
+                        $fecha = substr($fecha_completa, 0, 10);
+                        echo  $fecha . " - " .  $resena -> getNickname() ;
+
+                        echo '<div class="mi_resena_container" style="display:none;">Holaa</div>';
+
+                    echo "</li>";
+
+                }
+                echo "</ul>";
+            }
+        }
+        ?>
+    </section>
 </div>
 
 
