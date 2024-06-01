76e6e78474108be9b61d47bf2cf58eed206a9b05
jezulito96 <jesus.g.9696@gmail.com>
2024-06-02 01:05:23 +0200

    p

--- app/Views/confirmacion_email.php
+++ app/Views/confirmacion_email.php
@@ -6,7 +6,7 @@ if(isset($todo_guay)){
 
     <h2 class="bien">¡Email Confirmado!</h2>
     <img src="<?php echo base_url() . "img/email_confirm.jpg"; ?>" style="margin:0px auto;width:60vw" />
-    <p>Gracias por confirmar tu dirección de correo electrónico. Ahora tienes acceso completo a todas nuestras funcionalidades.</p>
+    <p style="padding:10px;margin-bottom:20px;">Gracias por confirmar tu dirección de correo electrónico. Ahora tienes acceso completo a todas nuestras funcionalidades.</p>
     <a style='margin-top:40px;margin-left:20px;' href='http://verifyReviews.es/verifyreviews/generarResenas' class='btn_enviar_largo' style='margin-left:20px'> 
         Ir al inicio
     </a>
