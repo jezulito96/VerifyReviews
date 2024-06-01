0619e24cdcdd67a8d060362869af5e6dc54c7da9
jezulito96 <jesus.g.9696@gmail.com>
2024-06-02 01:30:35 +0200

    p

--- app/Views/mi_negocio.php
+++ app/Views/mi_negocio.php
@@ -1,43 +1,48 @@
-<h1 style="padding-left:20px;">Mi negocio</h1>
+<div class="mi_negocio">
 
-<h5>Valoración</h5>
-<?php
-if(isset($error)){
-    echo $error;
-}else{
-  
-    if(isset($estadisticas)){
-        echo '<div class="container_valoracion">';
-            echo "<h6>Valoracion de tus clientes:</h6>" . "<h5>" . round($estadisticas['nota_media'],1) . " / 5</h5>";
-        echo '</div>';
+    <h1 style="padding-left:20px;">Mi negocio</h1>
+
+    <h5>Valoración</h5>
+    <?php
+    if(isset($error)){
+        echo $error;
+    }else{
     
-?>
-<h5>Ranking</h5>
-<?php print_r($estadisticas['ranking']); ?>
+        if(isset($estadisticas)){
+            echo '<div class="container_valoracion">';
+                echo "<h6>Valoracion de tus clientes:</h6>" . "<h5>" . round($estadisticas['nota_media'],1) . " / 5</h5>";
+            echo '</div>';
+        
+    ?>
+    <h5>Ranking</h5>
+    <?php print_r($estadisticas['ranking']); ?>
 
 
-<h5>Tus reseñas</h5>
-<?php
-    }
-
-    if(isset($lista_resenas)){
-        echo "<ul>";
-        foreach($lista_resenas as $i => $resena){
-
-            echo "<li class='mi_resena'>";
-                $fecha_completa = $resena -> getFechaServicio();
-                $fecha = substr($fecha_completa, 0, 10);
-                echo  $fecha . " - " .  $resena -> getNickname() ;
-
-                echo '<div class="mi_resena_container" style="display:none;">Holaa</div>';
-
-            echo "</li>";
-
+    <h5>Tus reseñas</h5>
+    <?php
         }
-        echo "</ul>";
+
+        if(isset($lista_resenas)){
+            echo "<ul>";
+            foreach($lista_resenas as $i => $resena){
+
+                echo "<li class='mi_resena'>";
+                    $fecha_completa = $resena -> getFechaServicio();
+                    $fecha = substr($fecha_completa, 0, 10);
+                    echo  $fecha . " - " .  $resena -> getNickname() ;
+
+                    echo '<div class="mi_resena_container" style="display:none;">Holaa</div>';
+
+                echo "</li>";
+
+            }
+            echo "</ul>";
+        }
     }
-}
-?>
+    ?>
+
+</div>
+
 
 <script>
     $(document).ready(function() {
