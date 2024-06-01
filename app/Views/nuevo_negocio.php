dcbb2f09c433c85567551c04728e66d3a99369c0
jezulito96 <jesus.g.9696@gmail.com>
2024-06-01 18:57:19 +0200

    p

--- app/Views/nuevo_negocio.php
+++ app/Views/nuevo_negocio.php
@@ -47,10 +47,10 @@
             <input  type="file" id="fotoPrincipal" name="fotoPrincipal" accept="image/*" required>
 
             <label for="sitio_web">Sitio Web:</label>
-            <input class="inputs" style="background:white;width:100%;" type="url" id="sitio_web" name="sitio_web" required>
+            <input class="inputs" type="url" id="sitio_web" name="sitio_web" required>
                 
             <label for="categoria">Categoría:</label>
-            <select class="inputs" id="categoria" name="categoria" required>
+            <select class="inputs" style="background:white;width:100%;" id="categoria" name="categoria" required>
                 <?php
                 if (isset($listaCategorias)) {
                     foreach ($listaCategorias as $i => $cat) {
