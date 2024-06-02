d61c201224f4c267719e64e6d32c7f4a8b183511
jezulito96 <jesus.g.9696@gmail.com>
2024-06-02 09:50:43 +0200

    p

--- app/Views/header_content.php
+++ app/Views/header_content.php
@@ -20,7 +20,7 @@
             src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABKElEQVR4nO2YzU7CQBRGz+PogldAZYE/rJSCO3CJoLj0kX0A5SeAlDShhpAZ2tA6nU6+k3TXuZmT2+nce0EIIYQQQvhIA3guMV4LuMExl8AXsAGGJcRrAt/AzKVMKhHvn9+CMlfAz0G8GXCNA/r7TMQlyLSB+VGsLfCGIxKZtWEDoxIkPnBMzyLzmmPtLbAwrJ1SERGwMmxofGLNnW8S58h4K5HStchM/t6Ae4vEO57RAZaWP1BtJFIeLZnJypaXPBgyU9k98V8ytZIIRqST49Py9pAHddifTmy4Nr/fbo7b3ftbPQqhRInOqIBtla/z8j2rjB/l7EW8kOkbJJIu8aVgYxUDnzgimFb34mj4UHSS0qxq+HAok0gMqOk4KKgBnRBCCCGEIJMdXBTwCqWDRf4AAAAASUVORK5CYII=" />
     </div>
 <!-- Lista que se desplegará al hacer clic en el botón -->
-<ul class="listaMenu" style="display: none">
+<ul class="listaMenu" >
     <?php
         $sesionIniciada = session() -> get("sesionIniciada");
         if(isset($sesionIniciada) && $sesionIniciada == 1) {
